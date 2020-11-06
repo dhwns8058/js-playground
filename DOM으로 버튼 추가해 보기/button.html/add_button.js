96a66113969441a058242d14f5fb1f435d4f256b
dhwns <dhwns8058@naver.com>
2020-11-06 22:52:19 +0900

    Dom버튼 연습

---
+++ DOM으로 버튼 추가해 보기/button.html/add_button.js
@@ -0,0 +1,12 @@
+
+var readInput = function(){
+    var input = document.getElementById("input1");
+    console.log(input.value);
+}
+
+var btn2 = document.createElement("button");
+btn2.innerHTML = "확인2";
+btn2.onclick = readInput;;
+var test = document.getElementById("text2");
+test.appendChild(document.createElement("br"))
+test.appendChild(btn2);
