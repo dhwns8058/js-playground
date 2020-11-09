d30ab4c9dadd10c163e04e5ed3e20238c25a8642
dhwns <dhwns8058@naver.com>
2020-11-09 14:35:32 +0900

    Update cal.js

--- 계산기구현/calc.js
+++ 계산기구현/calc.js
@@ -10,8 +10,15 @@ output.text = document.getElementById("output");
 var clickNumbers = function(event){
     var str = event.target.innerHTML;
 
-
-    // 공백 문자열 넣기
+    // 공백 문자열 넣기 (if)
+    if( str === "D"){
+        input.array.pop();
+    } else if (str === "+" || str === "-" || str === "*" || str === "/" ){
+        input.array.push(' ' + str + ' ')
+    } else (
+        input.array.push(str)
+    )
+    /* 공백 문자열 넣기(switch)
     switch(str){
         case 'D':
         input.array.pop();
@@ -24,7 +31,7 @@ var clickNumbers = function(event){
         break;
         default :
         input.array.push(str);
-    }
+    }*/
 
     // input.array 비어있을 때 
     if (input.array.length == 0){
